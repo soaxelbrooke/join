e2c81c4c064f0a9774e46461593339f996ccde25
Stuart Axel Owen <stuart.owen@gmail.com>
2015-04-28 05:52:13 +0000

    Add setup.cfg, update setup.py configuration

--- setup.py
+++ setup.py
@@ -3,8 +3,11 @@
 from distutils.core import setup
 
 setup(name='join',
-      version='0.0.1',
+      version='0.1',
       description='SQL-style joins for iterables.',
       author='Stuart Owen',
       author_email='stuart.owen@gmail.com',
+      url='https://github.com/StuartAxelOwen/join',
+      download_url='https://github.com/StuartAxelOwen/join/archive/0.1.zip',
+      keywords=['join', 'joins', 'merge', 'merges', 'list join', 'iterable join'],
       )
